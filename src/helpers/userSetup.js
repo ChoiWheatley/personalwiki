2a061d9593714b6026f6c82a1e92232f15323f2a
ChoiWheatley <chltmdgus604@naver.com>
2025-01-14 17:42:36 +0900

    addJavascriptFunction injectSpeedInsights

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,3 +1,5 @@
+const { injectSpeedInsights } = require("@vercel/speed-insights");
+
 function userMarkdownSetup(md) {
   // The md parameter stands for the markdown-it instance used throughout the site generator.
   // Feel free to add any plugin you want here instead of /.eleventy.js
@@ -5,6 +7,7 @@ function userMarkdownSetup(md) {
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+  eleventyConfig.addJavaScriptFunction("injectSpeedInsights", injectSpeedInsights);
 }
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
